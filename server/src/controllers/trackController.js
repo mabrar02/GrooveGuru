9e3fdc638c7f0f8ba39343830ef56a5a133339dd
mabrar02 <mahdeen02@gmail.com>
2024-02-27 17:42:16 -0400

    Adding to track router

--- server/src/controllers/trackController.js
+++ server/src/controllers/trackController.js
@@ -1,6 +1,82 @@
 require('dotenv').config();
 
 const currentlyPlayingEndpoint = 'https://api.spotify.com/v1/me/player/currently-playing';
+const recommendationsEndpoint = 'https://api.spotify.com/v1/recommendations';
+const featuresEndpoint = 'https://api.spotify.com/v1/audio-features/';
+
+const getSimilarFromTrack = async (req, res) => {
+    let token = req.query.token || null;
+    let trackId = req.query.trackId || null;
+
+    const params = new URLSearchParams();
+
+    if(token === null) {
+        res.json({
+            "error":"invalid_token"
+        });
+    }
+    else if(trackId === null){
+        res.json({
+            "error":"invalid_trackId"
+        });
+    }
+    else{
+        params.append('limit', process.env.SONG_REC_LIMIT);
+        params.append('seed_tracks', trackId);
+
+        const recUrl = `${recommendationsEndpoint}?${params.toString()}`;
+
+        fetch(recUrl, {
+            headers: {
+                Authorization:`Bearer ${token}`
+            }
+        }).then((response) => response.json())
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({"error":error});
+        });
+    }
+}
+
+const getSimilarFromGenre = async (req, res) => {
+    let token = req.query.token || null;
+    let genre = req.query.genre || null;
+
+    const params = new URLSearchParams();
+
+    if(token === null) {
+        res.json({
+            "error":"invalid_token"
+        });
+    }
+    else if(genre === null){
+        res.json({
+            "error":"invalid_genre"
+        });
+    }
+    else{
+        params.append('limit', process.env.SONG_REC_LIMIT);
+        params.append('seed_genres', genre);
+
+        const recUrl = `${recommendationsEndpoint}?${params.toString()}`;
+
+        fetch(recUrl, {
+            headers: {
+                Authorization:`Bearer ${token}`
+            }
+        }).then((response) => response.json())
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({"error":error});
+        });
+    }
+}
 
 const getCurrentTrack = async (req, res) => {
     let token = req.query.token || null;
@@ -17,7 +93,7 @@ const getCurrentTrack = async (req, res) => {
             }
         }).then((response) => response.json())
         .then((data) => {
-            //console.log(data.item.external_urls.spotify);
+            console.log(data.item.external_urls.spotify);
             if(!data || !data.is_playing){
                 res.json({
                     "error":"song not playing"
@@ -37,6 +113,100 @@ const getCurrentTrack = async (req, res) => {
     }
 }
 
+const getAudioFeatures = async (req, res) => {
+    let token = req.query.token || null;
+    let trackId = req.query.trackId || null;
+
+    if(token === null) {
+        res.json({
+            "error":"invalid_token"
+        });
+    }
+    else if(trackId === null){
+        res.json({
+            "error":"invalid_trackId"
+        });
+    }
+    else{
+        fetch(featuresEndpoint + trackId, {
+            headers: {
+                Authorization:`Bearer ${token}`
+            }
+        }).then((response) => response.json())
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({"error":error});
+        });
+    }
+}
+
+const getSimilarFromFeatures = async (req, res) => {
+    let token = req.query.token || null;
+    let trackId = req.query.trackId || null;
+
+    let energy = req.query.energy || null;
+    let acoustic = req.query.acoustic || null;
+    let dance = req.query.dance || null;
+    let popularity = req.query.popularity || null;
+    let mood = req.query.mood || null;
+
+    const params = new URLSearchParams();
+
+    if(token === null) {
+        res.json({
+            "error":"invalid_token"
+        });
+    }
+    else if(trackId === null){
+        res.json({
+            "error":"invalid_trackId"
+        });
+    }
+    else{
+        params.append('limit', process.env.SONG_REC_LIMIT);
+        params.append('seed_tracks', trackId);
+
+        if(energy != null){
+            params.append('target_energy', energy);
+        }
+        if(acoustic != null){
+            params.append('target_acousticness', acoustic);
+        }
+        if(dance != null){
+            params.append('target_danceability', dance);
+        }
+        if(popularity != null){
+            params.append('target_popularity', popularity);
+        }
+        if(mood != null){
+            params.append('target_valence', mood);
+        }
+
+        console.log(params.toString());
+
+        const recUrl = `${recommendationsEndpoint}?${params.toString()}`;
+
+        fetch(recUrl, {
+            headers: {
+                Authorization:`Bearer ${token}`
+            }
+        }).then((response) => response.json())
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({"error":error});
+        });
+    }
+}
 module.exports = {
-    getCurrentTrack
+    getCurrentTrack,
+    getSimilarFromTrack,
+    getSimilarFromGenre,
+    getAudioFeatures,
+    getSimilarFromFeatures
 }
--- server/src/routes/trackRouter.js
+++ server/src/routes/trackRouter.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const {getCurrentTrack} = require("../controllers/trackController");
+const {getCurrentTrack, getSimilarFromTrack, getSimilarFromGenre, getAudioFeatures, getSimilarFromFeatures} = require("../controllers/trackController");
 const router = express.Router()
 
 router.get('/current', getCurrentTrack);
+router.get('/features', getAudioFeatures);
+router.get('/rec/song', getSimilarFromTrack);
+router.get('/rec/genre', getSimilarFromGenre);
+router.get('/rec/features', getSimilarFromFeatures);
 
 module.exports = router
